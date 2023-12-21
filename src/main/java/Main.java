b48c55ba02437cc1baf4b2a8ca59ba991de0d865
pooja-2404 <108340987+pooja-2404@users.noreply.github.com>
2023-12-21 12:49:04 +0530

    Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,8 +12,8 @@ public class Main extends HttpServlet {
 			throws ServletException, IOException {
 		try {
 			String jdbcUrl = "jdbc:mysql://localhost:3306/db1";
-			String dbUser = "root";
-			String dbPassword = "Pooja@240494";
+			String dbUser = "";
+			String dbPassword = "";
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection conn = DriverManager.getConnection(jdbcUrl, dbUser, dbPassword);
 			getServletContext().setAttribute("dbConnection", conn);
